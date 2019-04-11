6a14db0681b17030052353e1fbddabf7badc58e9
Noam_Levy <noamlv@mta.ac.il>
2019-04-11 12:52:25 +0300

    added email and pass for profile

--- database/mongodb/models/profile.js
+++ database/mongodb/models/profile.js
@@ -2,12 +2,17 @@ const mongoose = require('mongoose');
 
 const profileSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name:{type:String,require:true},
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: { type: String, required: true },
     adminEvents:{type:String,require:true},
     attendingEvents:{type:String,require:true},
     pendingEvents:{type:String,require:true},
     age:Number
-
 });
 
 module.exports = mongoose.model('Profile', profileSchema);
